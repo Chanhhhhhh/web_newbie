5d82386972954b05c94b010b28f0c3493542e492
Chanhhhhhh <letrongfls2003@gmail.com>
2023-06-06 13:29:47 +0700

    remove public/css folder

--- src/index.js
+++ src/index.js
@@ -7,18 +7,23 @@ const handlebars = require('express-handlebars')
 const app = express();
 const port = 3000
 
+// khai bao anh join public, static files
+app.use(express.static(path.join(__dirname,'public')))
 // HTTP logger
 app.use(morgan('combined'));
 // template engine
-app.engine('hbs', handlebars({extname: '.hbs'}));
+// app.engine("handlebars",handlebars({defaultLayout:'main'}));
+app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-console.log(__dirname)
+// console.log(__dirname)
+app.get('/', (req, res) => {
+  res.render('new');
+})
 
-app.get('/newpiece', (req, res) => {
-  res.render('home');
-  // res.send(`<h1 style="color:red">Newpiece</h1>`)
+app.get('/oldpiece', (req, res) => {
+  res.render('old');
 })
 // dinh nghia tuyen duong trang web
 app.listen(port, () => {
