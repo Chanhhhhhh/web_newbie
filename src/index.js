616c96f4c2a9a4065192df19da4d161e80a4ebff
Chanhhhhhh <letrongfls2003@gmail.com>
2023-06-08 01:34:57 +0700

    form sumit GET/query parameter and POST/form data

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ const handlebars = require('express-handlebars')
 const app = express();
 const port = 3000
 
-// khai bao anh join public, static files
+// khai bao anh join public, static files(file tinh)
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({
+  extended: true}
+));
+app.use(express.json());
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 // template engine
 // app.engine("handlebars",handlebars({defaultLayout:'main'}));
 app.engine('hbs', handlebars.engine({extname: '.hbs'}));
@@ -21,11 +25,22 @@ app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
   res.render('new');
 })
-
+// dinh nghia tuyen duong trang web, basic route
 app.get('/oldpiece', (req, res) => {
   res.render('old');
 })
-// dinh nghia tuyen duong trang web
+
+app.get('/search', (req, res) => {
+  //  console.log(req.query); // trich xuat value  cua query parameter
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body.q); // lay du lieu tren form date
+  res.send('');
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
